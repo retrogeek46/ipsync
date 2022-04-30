72e30ce34fb89354ed8fce5279f71ce2a1264ae6
RetroGeek46 <divyangpradeep@gmail.com>
2022-05-01 02:00:58 +0530

    Add files

---
+++ api/views.py
@@ -0,0 +1,7 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+@api_view(['GET'])
+def getData(request):
+    ipData = {'ipAddress': '192.168.1.4'}
+    return Response(ipData)
